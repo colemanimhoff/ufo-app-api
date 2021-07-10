7ebe676e88eec7cb4faf80d33a9cc620a56b89c0
Coleman Imhoff <colemanimhoff@gmail.com>
2021-07-10 13:09:59 -0600

    adds express with test handler

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("what")
+const express = require('express');
+const app = express();
+const port = parseInt(process.env.PORT || 3031);
+
+app.get('/', (_, res) => {
+  res.json({
+    'message': 'Hayyyyyyyyy',
+  });
+});
+
+app.listen(port);
