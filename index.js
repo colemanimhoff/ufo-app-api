e28572a359e94e87dbc8309cdd78a8364d23f6f8
tharon <tharonweighill@gmail.com>
2021-07-10 13:42:44 -0600

    adds middleware

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 const port = parseInt(process.env.PORT || 3031);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(morgan(process.env.NODE_ENV !== 'production' ? 'dev' : 'combined'));
+app.use(cors({ origin: true, credentials: true }));
+
 app.get('/', (_, res) => {
-  res.json({
-    'message': 'Hayyyyyyyyy',
-  });
+    res.json({
+        'message': 'BISHrfgergergergerg',
+    });
 });
 
 app.listen(port);
